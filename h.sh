2984164ba6f746e6349c874d03145afbf67060a6
op30mmd <152014555+op30mmd@users.noreply.github.com>
2025-07-23 17:40:37 +0330

    Update h.sh

--- h.sh
+++ h.sh
@@ -8,33 +8,21 @@ monitor=,preferred,auto,auto
 exec-once = waybar & dunst & /usr/libexec/polkit-kde-authentication-agent-1 & hyprpaper
 exec-once = wl-paste --watch cliphist store
 
-# Source a file (multi-file configs)
-# source = ~/.config/hypr/myColors.conf
-
 # Set programs that you use
 $terminal = kitty
 $fileManager = thunar
 $menu = wofi --show drun
+$controlPanel = gnome-control-center
 
 # Some default env vars.
 env = XCURSOR_SIZE,24
-env = QT_QPA_PLATFORMTHEME,qt5ct # change to qt6ct if you have that
+env = QT_QPA_PLATFORMTHEME,qt5ct
 
 # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
 input {
     kb_layout = us
-    kb_variant =
-    kb_model =
-    kb_options =
-    kb_rules =
-
     follow_mouse = 1
-
-    touchpad {
-        natural_scroll = no
-    }
-
-    sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+    sensitivity = 0
 }
 
 general {
@@ -43,15 +31,12 @@ general {
     border_size = 2
     col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
     col.inactive_border = rgba(595959aa)
-
     layout = dwindle
-
     allow_tearing = false
 }
 
 decoration {
     rounding = 10
-    
     blur {
         enabled = true
         size = 3
@@ -61,9 +46,7 @@ decoration {
 
 animations {
     enabled = yes
-
     bezier = myBezier, 0.05, 0.9, 0.1, 1.05
-
     animation = windows, 1, 7, myBezier
     animation = windowsOut, 1, 7, default, popin 80%
     animation = border, 1, 10, default
@@ -89,14 +72,11 @@ misc {
     force_default_wallpaper = 0
 }
 
-device {
-    name = epic-mouse-v1
-    sensitivity = -0.5
-}
-
 windowrule = float,^(pavucontrol)$
 windowrule = float,^(blueman-manager)$
 windowrule = float,^(nm-connection-editor)$
+windowrule = float,^(wdisplays)$
+windowrule = float,^(gnome-control-center)$
 windowrule = float,title:^(Open File)$
 windowrule = float,title:^(Save File)$
 
@@ -109,16 +89,15 @@ bind = $mainMod, M, exit,
 bind = $mainMod, E, exec, $fileManager
 bind = $mainMod, V, togglefloating, 
 bind = $mainMod, D, exec, $menu
-bind = $mainMod, P, pseudo, # dwindle
-bind = $mainMod, J, togglesplit, # dwindle
+bind = $mainMod, S, exec, $controlPanel
+bind = $mainMod, P, pseudo,
+bind = $mainMod, J, togglesplit,
 
-# Move focus with mainMod + arrow keys
 bind = $mainMod, left, movefocus, l
 bind = $mainMod, right, movefocus, r
 bind = $mainMod, up, movefocus, u
 bind = $mainMod, down, movefocus, d
 
-# Switch workspaces with mainMod + [0-9]
 bind = $mainMod, 1, workspace, 1
 bind = $mainMod, 2, workspace, 2
 bind = $mainMod, 3, workspace, 3
@@ -130,7 +109,6 @@ bind = $mainMod, 8, workspace, 8
 bind = $mainMod, 9, workspace, 9
 bind = $mainMod, 0, workspace, 10
 
-# Move active window to a workspace with mainMod + SHIFT + [0-9]
 bind = $mainMod SHIFT, 1, movetoworkspace, 1
 bind = $mainMod SHIFT, 2, movetoworkspace, 2
 bind = $mainMod SHIFT, 3, movetoworkspace, 3
@@ -142,32 +120,24 @@ bind = $mainMod SHIFT, 8, movetoworkspace, 8
 bind = $mainMod SHIFT, 9, movetoworkspace, 9
 bind = $mainMod SHIFT, 0, movetoworkspace, 10
 
-# Scroll through existing workspaces with mainMod + scroll
 bind = $mainMod, mouse_down, workspace, e+1
 bind = $mainMod, mouse_up, workspace, e-1
 
-# Move/resize windows with mainMod + LMB/RMB and dragging
 bindm = $mainMod, mouse:272, movewindow
 bindm = $mainMod, mouse:273, resizewindow
 
-# Screenshots
 bind = , Print, exec, grim -g "$(slurp)" - | wl-copy
 bind = $mainMod, Print, exec, grim - | wl-copy
 
-# Lock screen
 bind = $mainMod, L, exec, hyprlock
-
-# Clipboard history
 bind = $mainMod, H, exec, cliphist list | wofi --dmenu | cliphist decode | wl-copy
 
-# Volume and Brightness controls
 binde=, XF86AudioRaiseVolume, exec, pactl set-sink-volume @DEFAULT_SINK@ +5%
 binde=, XF86AudioLowerVolume, exec, pactl set-sink-volume @DEFAULT_SINK@ -5%
 bind=, XF86AudioMute, exec, pactl set-sink-mute @DEFAULT_SINK@ toggle
 binde=, XF86MonBrightnessUp, exec, brightnessctl set +10%
 binde=, XF86MonBrightnessDown, exec, brightnessctl set 10%-
 
-# Media player controls
 bind=, XF86AudioPlay, exec, playerctl play-pause
 bind=, XF86AudioNext, exec, playerctl next
 bind=, XF86AudioPrev, exec, playerctl previous
